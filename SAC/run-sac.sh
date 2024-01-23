a93cffc69c9f15853c4c2b4c2004dbc97689e4db
rosa_wolf <wolf.5@campus.tu-berlin.de>
2024-01-23 17:23:38 +0100

    New runs

--- SAC/run-sac.sh
+++ SAC/run-sac.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-nohup python3 sac-train.py --env_name skill_conditioned_2x2_eval01 --term_on_change --reward_on_change --sparse --seed 1234567 > /dev/null 2>&1 &
-nohup python3 sac-train.py --env_name skill_conditioned_2x2_eval02 --term_on_change --reward_on_change --sparse --seed 5847836 > /dev/null 2>&1 &
-nohup python3 sac-train.py --env_name skill_conditioned_2x2_eval03 --term_on_change --reward_on_change --sparse --seed 8124099 > /dev/null 2>&1 &
-nohup python3 sac-train.py --env_name skill_conditioned_2x2_eval04 --term_on_change --reward_on_change --sparse --seed 2351294 > /dev/null 2>&1 &
-nohup python3 sac-train.py --env_name skill_conditioned_2x2_eval05 --term_on_change --reward_on_change --sparse --seed 9148235 > /dev/null 2>&1 &
+nohup  python3 sac-fm_training.py --env_name parallel_3x3 --num_skills 4 > /dev/null 2>&1 &
+nohup  python3 sac-fm_training.py --env_name parallel_3x3 --num_skills 2 > /dev/null 2>&1 &
+nohup  python3 sac-fm_training.py --env_name parallel_2x2 --num_skills 2 --relabeling > /dev/null 2>&1 &
