d37099527e621a6da57ed71c66ded9c1f9c9abee
rosa_wolf <wolf.5@campus.tu-berlin.de>
2024-01-29 15:42:55 +0100

    Added negative distance reward to parallel training

--- SAC/run-sac.sh
+++ SAC/run-sac.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
 nohup  python3 sac-fm_training.py --env_name parallel_3x3 --num_skills 4 > /dev/null 2>&1 &
-nohup  python3 sac-fm_training.py --env_name parallel_3x3 --num_skills 2 > /dev/null 2>&1 &
-nohup  python3 sac-fm_training.py --env_name parallel_2x2 --num_skills 2 --relabeling > /dev/null 2>&1 &
