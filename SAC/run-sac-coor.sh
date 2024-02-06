6590b598a067283c00c663280fc82eed05dfeb0f
rosa_wolf <wolf.5@campus.tu-berlin.de>
2024-02-06 14:49:01 +0100

    10 runs to compare obs spaces

---
+++ SAC/run-sac-coor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 123456 > /dev/null 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 487193 > /dev/null 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 495813 > /dev/null 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 582759 > /dev/null 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 918504 > /dev/null 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 285719 > /dev/null 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 126224 > /dev/null 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 214779 > /dev/null 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 978623 > /dev/null 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 682147 > /dev/null 2>&1 &
--- SAC/run-sac02.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-nohup  python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 123456 > /dev/null 2>&1 &
-nohup  python3 sac-train-old.py --env_name skill_conditioned_2x2_coord --num_epochs 2000 --dict_obs --give_coord --seed 487193 > /dev/null 2>&1 &
-nohup  python3 sac-train-old.py --env_name skill_conditioned_2x2_no-coord --num_epochs 2000 --dict_obs --seed 123456  > /dev/null 2>&1 &
-nohup  python3 sac-train-old.py --env_name skill_conditioned_2x2_no-coord --num_epochs 2000 --dict_obs --seed 487193 > /dev/null 2>&1 &
