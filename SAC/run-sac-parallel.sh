fff7b8b47e37e1bc46f0d5efd7c71ec1376f3966
rosa_wolf <wolf.5@campus.tu-berlin.de>
2024-02-09 13:29:17 +0100

    New runs for 2x3 environment

--- SAC/run-sac-parallel.sh
+++ SAC/run-sac-parallel.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 
-nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_no-coord --num_epochs 2000 --dict_obs --seed 123456 > /dev/null 2>&1 &
+nohup python3 sac-fm_training.py --env_name 2x3-new-penalty --num_skills 3 > /dev/null 2>&1 &
+nohup python3 sac-fm_training.py --env_name 2x3-new-penalty --num_skills 3 --relabeling > /dev/null 2>&1 &
+nohup python3 sac-fm_training.py --env_name 2x3-new-penalty --num_skills 3 --sparse > /dev/null 2>&1 &
+nohup python3 sac-fm_training.py --env_name 2x3-new-penalty --num_skills 3 --sparse --relabeling > /dev/null 2>&1 &
