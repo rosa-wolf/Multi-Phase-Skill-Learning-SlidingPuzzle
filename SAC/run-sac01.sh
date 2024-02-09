e0351d5f4db97bd0ea96a91bc9fc8e7188bd694f
rosa_wolf <wolf.5@campus.tu-berlin.de>
2024-02-09 16:50:45 +0100

    Changed file name in run fiel

--- SAC/run-sac01.sh
+++ SAC/run-sac01.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python3 sac-train-old.py --env_name 2x2_prio-buffer --num_epochs 2000 --sparse --seed 12345 > log_test.out 2>&1 &
+nohup python3 sac-train-old.py --env_name skill_conditioned_2x2_prio-buffer --num_epochs 2000 --sparse --seed 12345 > /dev/null 2>&1 &
