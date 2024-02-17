8694ffe9c656cacd06ad568fb5d54cae076b7957
rosa_wolf <wolf.5@campus.tu-berlin.de>
2024-02-17 13:18:59 +0100

    Movement evaluation for 2x3 puzzle

--- SAC/run-sac-move.sh
+++ SAC/run-sac-move.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 123456 --movement_reward > /dev/null 2>&1 &
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 487193 --movement_reward > /dev/null 2>&1 &
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 495813 --movement_reward > /dev/null 2>&1 &
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 582759 --movement_reward > /dev/null 2>&1 &
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 918504 --movement_reward > /dev/null 2>&1 &
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 285719 --movement_reward > /dev/null 2>&1 &
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 126224 --movement_reward > /dev/null 2>&1 &
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 214779 --movement_reward > /dev/null 2>&1 &
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 978623 --movement_reward > /dev/null 2>&1 &
-nohup python3 sac-train-new.py --env_name 3x3_4skills_predefined --num_epochs 20000 --dict_obs --seed 682147 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 123456 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 487193 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 495813 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 582759 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 918504 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 285719 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 126224 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 214779 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 978623 --movement_reward > /dev/null 2>&1 &
+nohup python3 sac-train-new.py --env_name 2x3_3skills_predefined --num_epochs 20000 --dict_obs --seed 682147 --movement_reward > /dev/null 2>&1 &
