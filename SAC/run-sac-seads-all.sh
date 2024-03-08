6a095e8753ef833b57ce707dc8ecf85864653adc
rosa_wolf <wolf.5@campus.tu-berlin.de>
2024-03-08 13:26:48 +0100

    Changed block size in 3x3 puzzle

--- SAC/run-sac-seads-all.sh
+++ SAC/run-sac-seads-all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-nohup python3 SEADS-train.py --env_name seads_2x3-relabel-fm --num_epochs 8000 --seed 123456 --num_skills 3 --num_episodes 20 --num_steps 100 --relabeling --novelty_bonus --second_best > /dev/null 2>&1 &
+nohup python3 SEADS-train.py --env_name seads_3x3 --num_epochs 10000 --seed 123456 --num_skills 4 --num_episodes 20 --num_steps 200 --novelty_bonus --second_best > /dev/null 2>&1 &
