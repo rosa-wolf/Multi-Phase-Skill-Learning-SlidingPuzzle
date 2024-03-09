80761147d9c04b1a2173488991118c27794dcc28
rosa_wolf <wolf.5@campus.tu-berlin.de>
2024-03-09 12:40:19 +0100

    Runs for many skills

--- SAC/run-sac-parallel-manyskills-noinit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-nohup python3 sac-fm_training.py --env_name 2x2-15skills-noinit --num_epochs 5000 --seed 578907 --num_skills 15 --num_episodes 20 --num_steps 100 --sparse --second_best --dorefinement > /dev/null 2>&1 &
-nohup python3 sac-fm_training.py --env_name 2x2-15skills-noinit --num_epochs 5000 --seed 937584 --num_skills 15 --num_episodes 20 --num_steps 100 --sparse --second_best --dorefinement > /dev/null 2>&1 &
-nohup python3 sac-fm_training.py --env_name 2x2-15skills-noinit --num_epochs 5000 --seed 105399 --num_skills 15 --num_episodes 20 --num_steps 100 --sparse --second_best --dorefinement > /dev/null 2>&1 &
-nohup python3 sac-fm_training.py --env_name 2x2-15skills-noinit --num_epochs 5000 --seed 195738 --num_skills 15 --num_episodes 20 --num_steps 100 --sparse --second_best --dorefinement > /dev/null 2>&1 &
---
+++ SAC/run-sac-seads-manyskills-5skills.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+nohup python3 SEADS-train.py --env_name seads_2x2-5skills --num_epochs 5000 --seed 578907 --num_skills 5 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
+nohup python3 SEADS-train.py --env_name seads_2x2-5skills --num_epochs 5000 --seed 937584 --num_skills 5 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
+nohup python3 SEADS-train.py --env_name seads_2x2-5skills --num_epochs 5000 --seed 105399 --num_skills 5 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
+nohup python3 SEADS-train.py --env_name seads_2x2-5skills --num_epochs 5000 --seed 195738 --num_skills 5 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
