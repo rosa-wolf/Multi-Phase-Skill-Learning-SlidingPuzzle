50215bf7b857747879299001d98628b4776e33b3
rosa_wolf <wolf.5@campus.tu-berlin.de>
2024-03-11 11:50:48 +0100

    Runs for 2x3 puzzle

--- SAC/run-sac-parallel-manyskills.sh
+++ SAC/run-sac-parallel-manyskills.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-nohup python3 sac-fm_training.py --env_name 2x2-10skills --num_epochs 5000 --seed 578907 --num_skills 10 --num_episodes 20 --num_steps 100 --sparse --second_best --doinit --dorefinement > /dev/null 2>&1 &
-nohup python3 sac-fm_training.py --env_name 2x2-10skills --num_epochs 5000 --seed 937584 --num_skills 10 --num_episodes 20 --num_steps 100 --sparse --second_best --doinit --dorefinement > /dev/null 2>&1 &
-nohup python3 sac-fm_training.py --env_name 2x2-10skills --num_epochs 5000 --seed 105399 --num_skills 10 --num_episodes 20 --num_steps 100 --sparse --second_best --doinit --dorefinement > /dev/null 2>&1 &
-nohup python3 sac-fm_training.py --env_name 2x2-10skills --num_epochs 5000 --seed 195738 --num_skills 10 --num_episodes 20 --num_steps 100 --sparse --second_best --doinit --dorefinement > /dev/null 2>&1 &
+nohup python3 sac-fm_training.py --env_name 2x3-10skills --num_epochs 7000 --seed 123456 --num_skills 10 --num_episodes 20 --num_steps 100 --sparse --second_best --doinit --dorefinement > /dev/null 2>&1 &
+nohup python3 sac-fm_training.py --env_name 2x3-10skills --num_epochs 7000 --seed 578907 --num_skills 10 --num_episodes 20 --num_steps 100 --sparse --second_best --doinit --dorefinement > /dev/null 2>&1 &
+nohup python3 sac-fm_training.py --env_name 2x3-10skills --num_epochs 7000 --seed 285910 --num_skills 10 --num_episodes 20 --num_steps 100 --sparse --second_best --doinit --dorefinement > /dev/null 2>&1 &
+nohup python3 sac-fm_training.py --env_name 2x3-10skills --num_epochs 7000 --seed 105399 --num_skills 10 --num_episodes 20 --num_steps 100 --sparse --second_best --doinit --dorefinement > /dev/null 2>&1 &
+nohup python3 sac-fm_training.py --env_name 2x3-10skills --num_epochs 7000 --seed 195738 --num_skills 10 --num_episodes 20 --num_steps 100 --sparse --second_best --doinit --dorefinement > /dev/null 2>&1 &
--- SAC/run-sac-seads-manyskills.sh
+++ SAC/run-sac-seads-manyskills.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-nohup python3 SEADS-train.py --env_name seads_2x2-15skills --num_epochs 5000 --seed 123456 --num_skills 15 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
-nohup python3 SEADS-train.py --env_name seads_2x2-15skills --num_epochs 5000 --seed 578907 --num_skills 15 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
-nohup python3 SEADS-train.py --env_name seads_2x2-15skills --num_epochs 5000 --seed 937584 --num_skills 15 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
-nohup python3 SEADS-train.py --env_name seads_2x2-15skills --num_epochs 5000 --seed 105399 --num_skills 15 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
-nohup python3 SEADS-train.py --env_name seads_2x2-15skills --num_epochs 5000 --seed 195738 --num_skills 15 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
+nohup python3 SEADS-train.py --env_name seads_2x3-10skills --num_epochs 7000 --seed 123456 --num_skills 10 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
+nohup python3 SEADS-train.py --env_name seads_2x3-10skills --num_epochs 7000 --seed 578907 --num_skills 10 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
+nohup python3 SEADS-train.py --env_name seads_2x3-10skills --num_epochs 7000 --seed 285910 --num_skills 10 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
+nohup python3 SEADS-train.py --env_name seads_2x3-10skills --num_epochs 7000 --seed 105399 --num_skills 10 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
+nohup python3 SEADS-train.py --env_name seads_2x3-10skills --num_epochs 7000 --seed 195738 --num_skills 10 --num_episodes 20 --num_steps 100 --novelty_bonus --second_best > /dev/null 2>&1 &
